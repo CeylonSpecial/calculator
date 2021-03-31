c939fd9ea74aaa8a3f92a1cd382fea2a82df5aa7
Cassandra <cassnydes@gmail.com>
2021-03-31 16:50:24 -0400

    added operator function

--- calculator.js
+++ calculator.js
@@ -25,3 +25,23 @@ function divide(...nums) {
         return total / num;
     });
 }
+
+function operate(num1, num2, operator) {
+
+    const operators = ['+','-','*','x','/'];
+
+    while (!operators.includes(operator)) {
+        operator = prompt('Sorry, that\'s not a valid operator. Please choose +, -, *, /');
+    }
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case 'x':
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
