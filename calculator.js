5eac170ab0f745cbe63d7e3a49f870605b7d1a9d
Cassandra <cassnydes@gmail.com>
2021-04-01 17:22:08 -0400

    connected calc html to javascript, added 0 and Clear buttons, rearranged button setup

--- calculator.js
+++ calculator.js
@@ -21,6 +21,12 @@ function multiply(...nums) {
 
 function divide(...nums) {
     
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === 0) {
+            return "Lol, nice try.";
+        };
+    }
+
     return nums.reduce((total, num) => {
         return total / num;
     });
@@ -47,44 +53,180 @@ function operate(num1, num2, operator) {
 }
 
 var selected = {
-    all: '',
-    num1: 0,
-    num2: 0,
+    num1: '',
+    num2: '',
     operator: '',
-    clearSelections() {
-        this.all = '';
-        this.num1 = 0;
-        this.num2 = 0;
-        this.operator = '';
+    solution: '',
+    progress: 0,
+    num1Cat(selection) {
+        this.num1 += selection;
+        this.progress = 1;
     },
-    parseSelections() {
-        this.num1 = 1;
-    }
-};
+    changeOperator(selection) {
+        this.operator = selection;
+        this.progress = 2;
+    },
+    num2Cat(selection) {
+        this.num2 += selection;
+        this.progress = 3;
+    },
+    clearSelections() {
+        this.num1 = '';
+        this.num2 = '';
+        this.operator = '';
+        this.progress = 0;
+    },
+    convertSelections() {
+        this.num1 = parseInt(this.num1);
+        this.num2 = parseInt(this.num2);
+    },
+    clearSolution() {
+        this.solution = '';
+    },
+    clearEntry() {
+        if (this.progress === 1) {
+            this.num1 = this.num1.substring(0, this.num1.length - 1);
+            if (this.num1.length === 0) {
+                this.progress = 0;
+            }
+        }
+        else if (this.progress === 2) {
+            this.operator = ''
+            this.progress = 1;
+            }
+        else if (this.progress === 3) {
+            this.num2 = this.num2.substring(0, this.num2.length - 1);
+            if (this.num2.length === 0) {
+                this.progress = 2;
+            }
+        }
+    },
+}
 
-function calcDisplay(selection) {
+function populateDisplay(newSelection) {
 
-    const operators = ['+','-','*','x','/'];
-    const numbers = ['1','2','3','4','5','6','7','8','9']
     const display = document.querySelector('#display');
 
-    if (operators.includes(selection) || numbers.includes(selection)) {
-        display.textContent += selection;
-        selected.all += selection;
-        console.log(selected.all);
+    display.textContent += newSelection;
+    return;
+}
+
+function clearDisplay() {
+    const display = document.querySelector('#display');
+
+    display.textContent = '';
+    return;
+}
+
+function clearLastEntry() {
+    const display = document.querySelector('#display');
+
+    if (display.textContent !== '') {
+        display.textContent = display.textContent.substring(0, display.textContent.length - 1);
+        return;
     }
-    else if (selection === 'Clear') {
-        display.textContent = '';
-        selected.clearSelections();
-    }
-    else {
+    return;    
+}
+
+function toggleClearButton() {
+    const clearButton = document.querySelector('#button-clear');
+    const display = document.querySelector('#display');
+
+    if (display.textContent === '' || display.textContent === selected.solution.toString()) {
+        clearButton.textContent = 'AC';
 
     }
+    else {
+        clearButton.textContent = 'CE';
+    }
+    return;
+}
+
+function checkSelection(newSelection) {
+    
+    const operators = ['+','-','*','x','/'];
+    const numbers = ['0','1','2','3','4','5','6','7','8','9'];
+    
+    if (newSelection === 'AC') {
+        clearDisplay();
+        selected.clearSelections();
+        selected.clearSolution();
+        toggleClearButton();
+        return;
+    }
+    else if (newSelection === 'CE') {
+        selected.clearEntry();
+        clearLastEntry();
+        toggleClearButton();
+        return;
+    }
+    else if (selected.solution === '') {
+        if (numbers.includes(newSelection) && selected.operator === '') {
+            selected.num1Cat(newSelection);
+        }
+        else if (operators.includes(newSelection) && selected.num1 !== '' && selected.operator === '') {
+            selected.changeOperator(newSelection);
+        }
+        else if (numbers.includes(newSelection)) {
+            selected.num2Cat(newSelection);
+        }
+        else if ((newSelection === '=' || operators.includes(newSelection)) && selected.num1 !== '' && selected.num2 !== '') {
+            selected.convertSelections();
+            selected.solution = operate(selected.num1, selected.num2, selected.operator);
+            selected.clearSelections();
+            if (operators.includes(newSelection)) {
+                selected.changeOperator(newSelection);
+                populateDisplay(newSelection);
+            }
+            else {
+                clearDisplay();
+                populateDisplay(selected.solution);
+                toggleClearButton();
+            }
+            return;
+        }
+        else {
+            return;
+        }
+    }
+    else {
+        if (numbers.includes(newSelection) && selected.operator === '') {
+            clearDisplay();
+            selected.clearSolution();
+            selected.num1Cat(newSelection);
+        }
+        else if (operators.includes(newSelection) && selected.operator === '') {
+            selected.changeOperator(newSelection);
+        }
+        else if (numbers.includes(newSelection)) {
+            selected.num1Cat(newSelection);
+        }
+        else if ((newSelection === '=' || operators.includes(newSelection)) && selected.num1 !== '' && selected.operator !== '') {
+            selected.convertSelections();
+            selected.solution = operate(selected.solution, selected.num1, selected.operator);
+            selected.clearSelections();
+            if (operators.includes(newSelection)) {
+                selected.changeOperator(newSelection);
+                populateDisplay(newSelection);
+            }
+            else {
+                clearDisplay();
+                populateDisplay(selected.solution);
+                toggleClearButton();
+            }
+            return;
+        }
+        else {
+            return;
+        }
+    }
+    populateDisplay(newSelection);
+    toggleClearButton();
     return;
 }
 
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach(button => button.addEventListener('click', () => {
-    calcDisplay(button.textContent);
+    checkSelection(button.textContent);
 }))
